48380b2d1b00eeb12f3e4eef8dcaedcdc02b3049
hunky <markovpavel32@gmail.com>
2019-08-14 12:13:44 +0300

    забыл добавить номера вот добавил)

--- src/components/GallaryRoot.vue
+++ src/components/GallaryRoot.vue
@@ -1,7 +1,7 @@
 <template functional>
   <div class="inline-flex flex-wrap w-full">
     <div v-for="(card, i) in props.list" :key="i" class="w-1/4 flex-auto border-2 m-2 bg-teal-100">
-      <div class="border-b-2 p-2">{{ card.title }}</div>
+      <div class="border-b-2 p-2">{{ card.title }} <span class="float-right text-green-400">{{ i + 1 }}</span></div>
       <div class="border-b-2 p-2">
         <div class="pl-3" v-for="attribute in card.attributes" :key="attribute">{{ attribute }}</div>
       </div>
